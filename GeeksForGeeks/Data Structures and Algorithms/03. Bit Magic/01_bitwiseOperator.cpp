8adcedebad27166641927b44b822ed816aefc456
Shafayet Sadi <shafayet.sadi@gmail.com>
2022-11-27 17:31:51 +0600

    .

--- GeeksForGeeks/Data Structures and Algorithms/03. Bit Magic/01_bitwiseOperator.cpp
+++ GeeksForGeeks/Data Structures and Algorithms/03. Bit Magic/01_bitwiseOperator.cpp
@@ -7,10 +7,36 @@ int main(){
 	freopen("output.txt", "w", stdout);
 #endif
 	
+	// Details is in my notebook
+
 	int x = 3;
 	int y = 6;
+	// Bitwise AND, OR, XOR
 	cout << (x&y) << endl;
 	cout << (x|y) << endl;
 	cout << (x^y) << endl;
+
+	// Left shift operator
+	cout << (x<<1) << endl;
+	cout << (x<<2) << endl;
+	cout << (x<<y) << endl;
+
+	// Right shift operator
+	x = 123;
+	cout << (x>>1) << endl;
+	cout << (x>>2) << endl;
+	cout << (x>>y) << endl;
+	
+	// Bitwise Not
+	unsigned int n = 1;
+	cout << (~n) << endl;
+	n = 5;
+	cout << (~n) << endl;
+
+	x = 1;
+	cout << (~x) << endl;
+	x = 5;
+	cout << (~x) << endl;
+
 	return 0;
 }	
