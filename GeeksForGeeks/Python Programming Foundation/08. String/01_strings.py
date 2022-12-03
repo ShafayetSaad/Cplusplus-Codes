d3800645ce76ada608c54efac3a4200aab49f2cc
Shafayet Sadi <shafayet.sadi@gmail.com>
2022-12-03 21:04:56 +0600

    .

--- GeeksForGeeks/Python Programming Foundation/08. String/01_strings.py
+++ GeeksForGeeks/Python Programming Foundation/08. String/01_strings.py
@@ -11,3 +11,25 @@ print(ord("a"))     # chr to ord
 print(ord("A"))
 print(chr(97))      # ord to chr
 print(chr(65))
+
+print()
+
+s = "geek"
+print(s)
+print(s[0])
+print(s[-1])
+print(s[1])
+print(s[-2])
+
+# Strings are immutable
+s = "geek"
+# s[0] = "e"  # error: item assignment not supported
+print(s)
+
+print()
+
+# We can also use 3 single quote
+s = """Hi,
+This is pythons course.
+Hope you are enjoying it."""
+print(s)
