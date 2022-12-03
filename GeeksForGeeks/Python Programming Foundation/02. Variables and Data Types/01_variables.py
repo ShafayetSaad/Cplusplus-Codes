5e9d69ee50aedd01a5f55f1779e3785dac3abe88
Shafayet Sadi <shafayet.sadi@gmail.com>
2022-12-03 12:04:18 +0600

    .

--- GeeksForGeeks/Python Programming Foundation/02. Variables and Data Types/01_variables.py
+++ GeeksForGeeks/Python Programming Foundation/02. Variables and Data Types/01_variables.py
@@ -5,11 +5,15 @@ print(age)
 print(name)
 print(weight)
 
+print("<------------------------>")
+
 x, y, z = 100, "geeks", 10.5
 print(x, y, z)
 
 x, y = x-5, y+"for"
 print(x, y, z)
+
+print("<------------------------>")
 
 # How variables work?
 x = 10 # x is reference to memory location value 10
@@ -17,6 +21,8 @@ y = "geek" # y is reference to memory location value geek
 z = x # z is reference to same memory location value 10
 z = 30 # z is reference to memory location value 30
 print(x, y, z)
+
+print("<------------------------>")
 
 # Python is Dynamically Typed
 x = 10
---
+++ GeeksForGeeks/Python Programming Foundation/08. String/01_strings.py
@@ -0,0 +1,13 @@
+"""
+Strings in Python
+- Sequence of characters
+- Used to store text data
+- Typically small set of characters
+- Characters 'A' to 'Z' are stored as values from 65 to 90
+- Characters 'a' to 'z' are stored as values from 97 to 122
+"""
+
+print(ord("a"))     # chr to ord
+print(ord("A"))
+print(chr(97))      # ord to chr
+print(chr(65))
