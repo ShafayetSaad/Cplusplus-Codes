0a93f8b9b340193fe1e26a5e09e68a16da191cf8
Shafayet Sadi <shafayet.sadi@gmail.com>
2022-12-19 20:04:41 +0600

    .

--- GeeksForGeeks/C++ STL/Vector/vector_01.cpp
+++ GeeksForGeeks/C++ STL/Vector/vector_01.cpp
@@ -13,7 +13,7 @@ int main(){
 	v.push_back(10);
 	v.push_back(20);
 	v.push_back(30);
-	cout << v.capacity() << endl;
+	// cout << v.capacity() << endl;
 
 	for(int i=0; i<v.size(); i++){
 		cout << v[i] << " ";
